2d67f859d4e4a5c432cf40eb1a1da35b5d0e6037
EC2 Default User <ec2-user@ip-172-31-36-129.us-east-2.compute.internal>
2020-01-31 01:36:25 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,35 @@
 
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    sorted = arr.sort.reverse
+    sorted[0] + sorted[1]
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.empty? || arr.length == 1
+    false
+  else
+    for i in 0..arr.length
+      for j in i..arr.length
+        if i + j == n
+          return true
+        end
+      end
+    end
+  end
+  false
 end
 
 # Part 2
